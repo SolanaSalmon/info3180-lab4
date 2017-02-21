3ea89be71876dc2ad7d7299aae4bbe3a7ef26acf
solanasalmon <solana_salmon@hotmail.com>
2017-02-21 17:46:09 +0000

    updated

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,11 @@
 from flask import Flask
+import os
+from flask import Flask, request, redirect, url_for
+from werkzeug.utils import secure_filename
+
+
+UPLOAD_FOLDER = './app/static/uploads'
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
 # Config Values
 USERNAME = 'admin'
@@ -9,4 +16,7 @@ SECRET_KEY = 'Sup3r$3cretkey'
 
 app = Flask(__name__)
 app.config.from_object(__name__)
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 from app import views
